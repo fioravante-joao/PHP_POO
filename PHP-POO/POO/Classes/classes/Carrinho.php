492f2d0620734045d6dc77f249b283b5436e79fd
João Fioravante <joao.fioravante@hotmail.com>
2019-05-11 11:58:09 -0300

    Classes
    Classes

---
+++ PHP-POO/POO/Classes/classes/Carrinho.php
@@ -0,0 +1,25 @@
+<?php
+
+class Carrinho
+{
+    private $itensCarrinho;
+
+    public function __construct()
+    {
+        $this->itensCarrinho = [];
+    }
+
+    public function adicionarItem(ItemCarrinho $item)
+    {
+        $this->itensCarrinho [] = $item;
+    }
+
+    public function total ()
+    {
+        $total = 0;
+        foreach($this->itensCarrinho as $item)
+        {
+            $total = $total +$item->subtotal();
+        }
+    }
+}
